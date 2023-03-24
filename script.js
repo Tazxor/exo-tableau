543f9a46e06530dabf9f067252046948a2c76c62
Tazxor <t.ozgul@hotmail.fr>
2023-03-24 09:39:36 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let amis = ['stephane','pierre','vitor']
+console.log(amis[0])// exo1
+
+let nombre = [1,2,3,4,5]
+for (let i = 0; i < nombre.length; i++) {
+   console.log(nombre[i])
+    
+}//exo2
+
+let nombre2 = [1,2,3,4,5]
+nombre2.push(6);
+console.log(nombre2)//exo3
+
+
+let nombre3 = [1,2,3,4,5]
+nombre3.pop();
+console.log(nombre3)//exo4
+
+
+
+
+let nombre4 = [1,2,3,4,5]
+nombre4.unshift(0);
+console.log(nombre4)//exo5
+
+
+let nombre5 = [1,2,3,4,5]
+nombre5.shift();
+console.log(nombre5)//exo6
